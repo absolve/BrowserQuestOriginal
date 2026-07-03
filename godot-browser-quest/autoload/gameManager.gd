96b03dddd25baf82c5f2373906448e9b1451eea3
absolve <wangding1992@126.com>
2026-07-03 16:40:44 +0800

    更新功能

--- godot-browser-quest/autoload/gameManager.gd
+++ godot-browser-quest/autoload/gameManager.gd
@@ -2,131 +2,131 @@ extends Node
 #保存游戏信息
 
 #实体类型
-enum Entities{
-		WARRIOR= 1,        # 战士（玩家角色）
+enum Entities {
+		WARRIOR = 1, # 战士（玩家角色）
 		
 		# Mobs - 怪物类型
-		RAT= 2,            # 老鼠
-		SKELETON= 3,       # 骷髅
-		GOBLIN= 4,         # 哥布林
-		OGRE= 5,           # 食人魔
-		SPECTRE= 6,        # 幽灵
-		CRAB= 7,           # 螃蟹
-		BAT= 8,            # 蝙蝠
-		WIZARD= 9,         # 巫师
-		EYE= 10,           # 眼魔
-		SNAKE= 11,         # 蛇
-		SKELETON2= 12,     # 骷髅战士
-		BOSS= 13,          # BOSS
-		DEATHKNIGHT= 14,   # 死亡骑士
+		RAT = 2, # 老鼠
+		SKELETON = 3, # 骷髅
+		GOBLIN = 4, # 哥布林
+		OGRE = 5, # 食人魔
+		SPECTRE = 6, # 幽灵
+		CRAB = 7, # 螃蟹
+		BAT = 8, # 蝙蝠
+		WIZARD = 9, # 巫师
+		EYE = 10, # 眼魔
+		SNAKE = 11, # 蛇
+		SKELETON2 = 12, # 骷髅战士
+		BOSS = 13, # BOSS
+		DEATHKNIGHT = 14, # 死亡骑士
 		
 		# Armors - 护甲类型
-		FIREFOX= 20,       # 火狐护甲（无敌状态）
-		CLOTHARMOR= 21,    # 布甲
-		LEATHERARMOR= 22,  # 皮甲
-		MAILARMOR= 23,     # 锁甲
-		PLATEARMOR= 24,    # 板甲
-		REDARMOR= 25,      # 红甲
-		GOLDENARMOR= 26,   # 金甲
+		FIREFOX = 20, # 火狐护甲（无敌状态）
+		CLOTHARMOR = 21, # 布甲
+		LEATHERARMOR = 22, # 皮甲
+		MAILARMOR = 23, # 锁甲
+		PLATEARMOR = 24, # 板甲
+		REDARMOR = 25, # 红甲
+		GOLDENARMOR = 26, # 金甲
 		
 		# Objects - 物品类型
-		FLASK= 35,         # 药水瓶
-		BURGER= 36,        # 汉堡
-		CHEST= 37,         # 宝箱
-		FIREPOTION= 38,    # 火焰药水
-		CAKE= 39,          # 蛋糕
+		FLASK = 35, # 药水瓶
+		BURGER = 36, # 汉堡
+		CHEST = 37, # 宝箱
+		FIREPOTION = 38, # 火焰药水
+		CAKE = 39, # 蛋糕
 		
 		# NPCs - 非玩家角色类型
-		GUARD= 40,         # 卫兵
-		KING= 41,          # 国王
-		OCTOCAT= 42,       # Octocat
-		VILLAGEGIRL= 43,   # 村庄女孩
-		VILLAGER= 44,      # 村民
-		PRIEST= 45,        # 牧师
-		SCIENTIST= 46,     # 科学家
-		AGENT= 47,         # 特工
-		RICK= 48,          # Rick
-		NYAN= 49,          # Nyan Cat
-		SORCERER= 50,      # 巫师
-		BEACHNPC= 51,      # 海滩NPC
-		FORESTNPC= 52,     # 森林NPC
-		DESERTNPC= 53,     # 沙漠NPC
-		LAVANPC= 54,       # 熔岩NPC
-		CODER= 55,         # 程序员
+		GUARD = 40, # 卫兵
+		KING = 41, # 国王
+		OCTOCAT = 42, # Octocat
+		VILLAGEGIRL = 43, # 村庄女孩
+		VILLAGER = 44, # 村民
+		PRIEST = 45, # 牧师
+		SCIENTIST = 46, # 科学家
+		AGENT = 47, # 特工
+		RICK = 48, # Rick
+		NYAN = 49, # Nyan Cat
+		SORCERER = 50, # 巫师
+		BEACHNPC = 51, # 海滩NPC
+		FORESTNPC = 52, # 森林NPC
+		DESERTNPC = 53, # 沙漠NPC
+		LAVANPC = 54, # 熔岩NPC
+		CODER = 55, # 程序员
 		
 		# Weapons - 武器类型
-		SWORD1= 60,        # 铁剑
-		SWORD2= 61,        # 钢剑
-		REDSWORD= 62,      # 红剑
-		GOLDENSWORD= 63,   # 金剑
-		MORNINGSTAR= 64,   # 晨星锤
-		AXE= 65,           # 斧头
-		BLUESWORD= 66      # 蓝剑
+		SWORD1 = 60, # 铁剑
+		SWORD2 = 61, # 钢剑
+		REDSWORD = 62, # 红剑
+		GOLDENSWORD = 63, # 金剑
+		MORNINGSTAR = 64, # 晨星锤
+		AXE = 65, # 斧头
+		BLUESWORD = 66 # 蓝剑
 		}
 		
 #方向		
-enum Orientations{ 
-			UP= 1,             # 向上
-			DOWN= 2,           # 向下
-			LEFT= 3,           # 向左
-			RIGHT= 4           # 向右
-		}		
+enum Orientations {
+			UP = 1, # 向上
+			DOWN = 2, # 向下
+			LEFT = 3, # 向左
+			RIGHT = 4 # 向右
+		}
 		
 		
 var kinds = {
-	Entities.WARRIOR:  "player",
+	Entities.WARRIOR: "player",
 	
-	Entities.RAT:  "mob",
-	Entities.SKELETON:  "mob",
-	Entities.GOBLIN:  "mob",
-	Entities.OGRE:  "mob",
-	Entities.SPECTRE:  "mob",
-	Entities.DEATHKNIGHT:  "mob",
+	Entities.RAT: "mob",
+	Entities.SKELETON: "mob",
+	Entities.GOBLIN: "mob",
+	Entities.OGRE: "mob",
+	Entities.SPECTRE: "mob",
+	Entities.DEATHKNIGHT: "mob",
 	Entities.CRAB: "mob",
-	Entities.SNAKE:  "mob",
-	Entities.BAT:  "mob",
-	Entities.WIZARD:  "mob",
-	Entities.EYE:  "mob",
-	Entities.SKELETON2:  "mob",
+	Entities.SNAKE: "mob",
+	Entities.BAT: "mob",
+	Entities.WIZARD: "mob",
+	Entities.EYE: "mob",
+	Entities.SKELETON2: "mob",
 	Entities.BOSS: "mob",
 
-	Entities.SWORD1:  "weapon",
-	Entities.SWORD2:  "weapon",
-	Entities.AXE:  "weapon",
-	Entities.REDSWORD:  "weapon",
+	Entities.SWORD1: "weapon",
+	Entities.SWORD2: "weapon",
+	Entities.AXE: "weapon",
+	Entities.REDSWORD: "weapon",
 	Entities.BLUESWORD: "weapon",
-	Entities.GOLDENSWORD:  "weapon",
-	Entities.MORNINGSTAR:  "weapon",
+	Entities.GOLDENSWORD: "weapon",
+	Entities.MORNINGSTAR: "weapon",
 	
-	Entities.FIREFOX:  "armor",
+	Entities.FIREFOX: "armor",
 	Entities.CLOTHARMOR: "armor",
-	Entities.LEATHERARMOR:  "armor",
-	Entities.MAILARMOR:  "armor",
-	Entities.PLATEARMOR:  "armor",
-	Entities.REDARMOR:  "armor",
-	Entities.GOLDENARMOR:  "armor",
+	Entities.LEATHERARMOR: "armor",
+	Entities.MAILARMOR: "armor",
+	Entities.PLATEARMOR: "armor",
+	Entities.REDARMOR: "armor",
+	Entities.GOLDENARMOR: "armor",
 
-	Entities.FLASK:  "object",
-	Entities.CAKE:  "object",
+	Entities.FLASK: "object",
+	Entities.CAKE: "object",
 	Entities.BURGER: "object",
-	Entities.CHEST:  "object",
-	Entities.FIREPOTION:  "object",
+	Entities.CHEST: "object",
+	Entities.FIREPOTION: "object",
 
-	Entities.GUARD:  "npc",
-	Entities.VILLAGEGIRL:  "npc",
-	Entities.VILLAGER:  "npc",
-	Entities.CODER:  "npc",
+	Entities.GUARD: "npc",
+	Entities.VILLAGEGIRL: "npc",
+	Entities.VILLAGER: "npc",
+	Entities.CODER: "npc",
 	Entities.SCIENTIST: "npc",
-	Entities.PRIEST:  "npc",
-	Entities.KING:  "npc",
-	Entities.RICK:  "npc",
+	Entities.PRIEST: "npc",
+	Entities.KING: "npc",
+	Entities.RICK: "npc",
 	Entities.NYAN: "npc",
-	Entities.SORCERER:  "npc",
-	Entities.AGENT:  "npc",
-	Entities.OCTOCAT:  "npc",
+	Entities.SORCERER: "npc",
+	Entities.AGENT: "npc",
+	Entities.OCTOCAT: "npc",
 	Entities.BEACHNPC: "npc",
-	Entities.FORESTNPC:  "npc",
-	Entities.DESERTNPC:  "npc",
+	Entities.FORESTNPC: "npc",
+	Entities.DESERTNPC: "npc",
 	Entities.LAVANPC: "npc",
    
 };
--- godot-browser-quest/autoload/networkManager.gd
+++ godot-browser-quest/autoload/networkManager.gd
@@ -3,40 +3,53 @@ extends Node
 
 
 #消息类型
-enum Messages{
-		HELLO= 0,          # 客户端握手消息
-		WELCOME= 1,        # 服务器欢迎消息
-		SPAWN= 2,          # 实体生成消息
-		DESPAWN= 3,        # 实体消失消息
-		MOVE= 4,           # 移动消息
-		LOOTMOVE= 5,       # 拾取移动消息
-		AGGRO= 6,          # 仇恨消息
-		ATTACK= 7,         # 攻击消息
-		HIT= 8,            # 命中消息
-		HURT= 9,           # 受伤消息
-		HEALTH= 10,        # 生命值消息
-		CHAT= 11,          # 聊天消息
-		LOOT= 12,          # 拾取消息
-		EQUIP= 13,         # 装备消息
-		DROP= 14,          # 掉落消息
-		TELEPORT= 15,      # 传送消息
-		DAMAGE= 16,        # 伤害消息
-		POPULATION= 17,    # 人口统计消息
-		KILL= 18,          # 击杀消息
-		LIST= 19,          # 实体列表消息
-		WHO= 20,           # 查询在线玩家消息
-		ZONE= 21,          # 区域切换消息
-		DESTROY= 22,       # 销毁消息
-		HP= 23,            # 最大生命值消息
-		BLINK= 24,         # 闪烁消息
-		OPEN= 25,          # 打开消息（宝箱）
-		CHECK= 26         # 检查点消息
+enum Messages {
+		HELLO = 0, # 客户端握手消息
+		WELCOME = 1, # 服务器欢迎消息
+		SPAWN = 2, # 实体生成消息
+		DESPAWN = 3, # 实体消失消息
+		MOVE = 4, # 移动消息
+		LOOTMOVE = 5, # 拾取移动消息
+		AGGRO = 6, # 仇恨消息
+		ATTACK = 7, # 攻击消息
+		HIT = 8, # 命中消息
+		HURT = 9, # 受伤消息
+		HEALTH = 10, # 生命值消息
+		CHAT = 11, # 聊天消息
+		LOOT = 12, # 拾取消息
+		EQUIP = 13, # 装备消息
+		DROP = 14, # 掉落消息
+		TELEPORT = 15, # 传送消息
+		DAMAGE = 16, # 伤害消息
+		POPULATION = 17, # 人口统计消息
+		KILL = 18, # 击杀消息
+		LIST = 19, # 实体列表消息
+		WHO = 20, # 查询实体信息根据ID
+		ZONE = 21, # 区域切换消息
+		DESTROY = 22, # 销毁消息
+		HP = 23, # 最大生命值消息
+		BLINK = 24, # 闪烁消息
+		OPEN = 25, # 打开消息（宝箱）
+		CHECK = 26 # 检查点消息
 		}
+
+# 映射 Messages 枚举值到其名称字符串（索引即枚举值）
+var MESSAGE_NAMES = [
+	"HELLO", "WELCOME", "SPAWN", "DESPAWN", "MOVE", "LOOTMOVE", "AGGRO", "ATTACK", "HIT", "HURT",
+	"HEALTH", "CHAT", "LOOT", "EQUIP", "DROP", "TELEPORT", "DAMAGE", "POPULATION", "KILL", "LIST",
+	"WHO", "ZONE", "DESTROY", "HP", "BLINK", "OPEN", "CHECK"
+]
+
+func get_message_name(value: int) -> String:
+	if typeof(value) == TYPE_INT:
+		if value >= 0 and value < MESSAGE_NAMES.size():
+			return MESSAGE_NAMES[value]
+	return "UNKNOWN"
 
 
 @onready var client: SocketIO = $SocketIO
 
-var isConnected=false
+var isConnected = false
 
 
 func _ready() -> void:
@@ -48,49 +61,52 @@ func _ready() -> void:
 
 #连接到服务器
 func connectServer():
-	client.base_url="http://localhost:8000"
+	client.base_url = "http://localhost:8000"
 	#client.socket_path=""
 	client.connect_socket()
 	
 #发送消息
 func sendMessage(data):
 	if isConnected:
-		client.emit("message",data)
+		client.emit("message", data)
 
 #发送玩家登录信息
-func sendHello(playerName,armor,weapon):
-	sendMessage([Messages.HELLO,playerName,armor,weapon])
+func sendHello(playerName, armor, weapon):
+	sendMessage([Messages.HELLO, playerName, armor, weapon])
+
+
+
 
 func _on_socket_connected(ns: String) -> void:
 	#client.emit("hello")
 	#client.emit("some_event", { "value": 10 })
-	print('_on_socket_connected',ns)
-	isConnected=true
+	print('_on_socket_connected', ns)
+	isConnected = true
 	pass
 	
 func _on_event_received(event: String, data: Variant, _ns: String) -> void:
 	#print("event %s with %s as data received" % [event, data])
 	Log.debug("event %s with %s as data received " % [event, data])
-	if event=='message':
+	if event == 'message':
 		if data is Array:
 			if !data.is_empty():
 				if data[0] is String:
-					if data[0]=='go':
-						sendHello("1",21,60)
-					elif data[0]=='timeout':
+					if data[0] == 'go':
+						sendHello("1", 21, 60)
+					elif data[0] == 'timeout':
 						pass
-				elif data[0] is Array:  #数据包
-					if !data[0].is_empty() && data[0][0] is Array:  #批量消息
-						
+				elif data[0] is Array: # 数据包
+					if !data[0].is_empty() && data[0][0] is Array: # 批量消息
 						pass
-					elif !data[0].is_empty():  #单个消息
+					elif !data[0].is_empty(): # 单个消息
+						Log.debug(get_message_name(data[0][0]))
 						
 						pass
 					else:
-						Log.error(event,'未知消息')	
+						Log.error(event, '未知消息')
 	else:
-		Log.error(event,'未知事件')	
+		Log.error(event, '未知事件')
 
 func _on_socket_io_socket_disconnected() -> void:
-	isConnected=false
+	isConnected = false
 	pass # Replace with function body.
